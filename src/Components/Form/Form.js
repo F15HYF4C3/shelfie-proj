96a84113f813cbd22226767b21150ecaf84e328f
Tyra Muhlestein <tmuhlestein@xpressbillpay.net>
2019-05-18 14:41:31 -0600

    Form Styling\functionality

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
+import './Form.css';
+// import Axios from 'axios';
 
 class Form extends Component {
     render(){
+        // const imageUrl = "";
+        // const pimageurlForm = ()=>{
+        //     Axios.get(`http://${imageUrl}`)
+    
+        // }
         return (
             <div>
-                Form
+                <div>Form</div>
+                <div>
+                    New Product
+                    Item Price
+                    Image Address
+                </div>
+                <input type="text" className="pnameForm"/>
+                <input type="text" className="priceForm"/>
+                <input type="text" className="pimageurlForm"/>
+                <div className="imgPreview">
+                <div>New Product Image</div>
+                <div className="imgBox"></div>
+                </div>
             </div>
         )
     }
-
+// {pimageurlForm}
 }
 
 export default Form
