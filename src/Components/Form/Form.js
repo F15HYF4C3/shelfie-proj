cfb68725d1a01569ecb8cbebb89a8b66bd7e8e85
Tyra M <t.nicole.education@gmail.com>
2019-05-22 21:05:39 -0600

    Working on Form.js Format

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,8 +2,13 @@ import React, {Component} from 'react';
 import './Form.css';
 // import Axios from 'axios';
 
+
 class Form extends Component {
-    render(){
+   
+        render(){
+
+     
+    
         // const imageUrl = "";
         // const pimageurlForm = ()=>{
         //     Axios.get(`http://${imageUrl}`)
@@ -11,24 +16,42 @@ class Form extends Component {
         // }
         return (
             <div>
+                {/* onHandleClick(Button){
+    app.post('/api/pname', (req, res, next)=>{
+        const prodsdb = req.app.get('db');
+        const {pname, pprice, pimageurl} = req.body;
+        //can use this function in any other files
+        prodsdb.productdb.insert({
+            pname, 
+            pprice, 
+            pimageurl
+        })
+        .then((pname)=>{
+       res.send(pname)
+        })
+    })
+}  */}
+  
                 <div>Form</div>
                 <div display="inline" className="inputTitles">
-                   <p>New Product</p> 
-                   <p>Item Price</p>
-                   <p>Image Address</p>                  
-                    
+               <input type="text" name="New Product" id="pnameForm"/>
+                <input type="text" name="Item Price" id="priceForm"/>
+                <input type="text" name="Image Address" id="pimageurlForm"/>
                 </div>
-                <input type="text" className="pnameForm"/>
-                <input type="text" className="priceForm"/>
-                <input type="text" className="pimageurlForm"/>
+            
+                
+                
                 <div className="imgPreview">
                 <div>New Product Image</div>
                 <div className="imgBox"></div>
+                <button>Add to Inventory</button>
+                   <button>Cancel</button> 
                 </div>
             </div>
         )
     }
 // {pimageurlForm}
 }
+
 
 export default Form
